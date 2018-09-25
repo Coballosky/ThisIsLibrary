e591298fb821b99ecf9c3adae0893e7c1708632f
NikkWolf <nicolas.fuentealba.brito@gmail.com>
2018-09-25 14:00:05 -0300

    a

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,10 +11,20 @@ import java.io.IOException;
 import Comun.Metodos;
 import ConnectionHandler.SqlConection;
 
-
+/**
+ * 
+ * @author Nicolas
+ *
+ *
+ * Este programa corre con base de datos MYSQL externa, para comunicar hacia tal database se requiere
+ * del uso del puerto 3306 por defecto, desconosco si a la hora de entrega arreglamos ese tema cambiando el puerto 
+ * o con otra solucion. porfavor tener en cuenta.
+ *
+ *
+ */
 
 public class Main extends Application {
-	
+	private String path = "c://POO//SqlData.txt" ;
 	
 	@Override
 	public void start(Stage primaryStage) throws IOException {
@@ -43,12 +53,11 @@ public class Main extends Application {
 	public void SetupSQL() throws IOException {
 		Metodos Met = new Metodos();
 		SqlConection SQL = new SqlConection();
-		File tmpDir = new File("c://POO//SqlData.txt");
+		File tmpDir = new File(path);
 		if (tmpDir.exists()) {
 			SQL.LoadConData();
 		}else {
 			try {
-			
 			Stage ventanaSQL = new Stage();
 			Parent sql = FXMLLoader.load(getClass().getResource("/Views/ConfigSQL.fxml"));
 			Scene escena = new Scene(sql);
